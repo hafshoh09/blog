6a3c322f68b18d6c7055d43f76b6614aa1fcc2a4
Rizki Fahrezi Maulana <66943757+Ikiezi990@users.noreply.github.com>
2022-06-19 12:25:20 +0700

    ok

--- resources/views/viewPost.blade.php
+++ resources/views/viewPost.blade.php
@@ -3,29 +3,45 @@
 @section('content')
     {{-- Artikel --}}
     <div class="wrapper">
-        <div class="container">
-            <div class="container">
-
-                <h1 class="title">{{ $post->judul_artikel }}</h1>
-                <h4 class="text-right">Published At : {{ $post->created_at }}</h4>
-                <h4 class="text-right">Kategori : {{ $post->kategori->nama_kategori }}</h4>
-            </div>
-            <div class="inner-wrapper mb-2 mt-3">
+        <div class="row">
+            <div class="col-md-9">
                 <div class="container">
-                    <div class="card">
-                        <div class="card-body">
 
-                            {!! $post->isi_artikel !!}
+                    <div class="inner-wrapper mb-2 mt-3">
+                        <div class="container">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h1 class="title">{{ $post->judul_artikel }}</h1>
+                                    <h4 class="text-right">Published At : {{ $post->created_at }}</h4>
+                                    <h4 class="text-right">Kategori : {{ $post->kategori->nama_kategori }}</h4>
+                                    {!! $post->isi_artikel !!}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            {{-- Komentar --}}
-            <div class="container">
+            <div class="col-md-3 mb-2 mt-3 ">
                 <div class="container">
 
-                    <h4>Komentar</h4>
-                    <textarea id="mymce" name="isi_artikel" value="{{ old('isi_artikel') }}" class="form-control @error('isi_artikel') is-invalid @enderror"></textarea>
+                    <div class="card">
+                        <div class="card-body">
+                            Profil
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                {{-- Komentar --}}
+                <div class="container">
+                    <div class="container">
+
+                        <h4>Komentar</h4>
+                        <textarea id="mymce" name="isi_artikel" value="{{ old('isi_artikel') }}" class="form-control @error('isi_artikel') is-invalid @enderror"></textarea>
+                    </div>
                 </div>
             </div>
         </div>
